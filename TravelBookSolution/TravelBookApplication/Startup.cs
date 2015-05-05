1950896cd4f755bd9a7e78117b6b8ea28b7c8386
karieiriks <kari14@ru.is>
2015-05-05 11:03:03 +0000

    Tók út og setti inn project

---
+++ TravelBookSolution/TravelBookApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelBookApplication.Startup))]
+namespace TravelBookApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
