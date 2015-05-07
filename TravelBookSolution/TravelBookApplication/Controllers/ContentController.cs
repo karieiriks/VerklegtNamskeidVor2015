939916405fc406d808ce32bcdc0937ab12d63210
Bjoggi79 <bjorgvinj12@ru.is>
2015-05-07 10:59:14 +0000

    Bætti við controller

--- TravelBookSolution/TravelBookApplication/Controllers/ContentController.cs
+++ TravelBookSolution/TravelBookApplication/Controllers/ContentController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace TravelBookApplication.Controllers
 {
-    public class ContentController
+    public class ContentController : Controller
     {
     }
 }
