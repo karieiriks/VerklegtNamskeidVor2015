058992d9fbcc0bf4110704c29bfebd9b2d67277d
unknown <sverrir1993@gmail.com>
2015-05-11 00:14:55 +0000

    Added file validation via javascript on the file input of the newsfeed form

--- TravelBookSolution/TravelBookApplication/Scripts/newsfeed-widgets.js
+++ TravelBookSolution/TravelBookApplication/Scripts/newsfeed-widgets.js
@@ -1,5 +1,15 @@
 ﻿$(document).ready(function () {
 
+    var clearFileInput = function () {
+        $("#post-image-input").val("");
+        $("#file-clear").addClass("hidden");
+        $(".file-name-section").html("");
+    }
+
+    var addFileInputError = function (error) {
+        $("#file-input-error").html(error);
+    }
+
     $(".left-widgets, .right-widgets").click(function (e) {
         e.preventDefault();
     })
@@ -20,8 +30,8 @@
     })
 
     $("#post-image-input").change(function () {
+        addFileInputError("");
         var filename = $(this).val();
-
         if (filename == "")
         {
             $("#file-clear").addClass("hidden");
@@ -32,16 +42,22 @@
         }
 
         $(".file-name-section").html(filename);
+
+        if (filename.length > 150) {
+            clearFileInput();
+            addFileInputError("This file name is too long");
+        }
+        else if (this.files[0].size > 4000000) {
+            clearFileInput();
+            addFileInputError("This image is too large");
+        }
     })
 
     $("#file-clear").click(function () {
-        $("#post-image-input").val("");
-        $(this).addClass("hidden");
-        $(".file-name-section").html("");
-
-    })
+        clearFileInput();
+    });
 
     $("#post-button").click(function () {
 
-    })
+    });
 })
