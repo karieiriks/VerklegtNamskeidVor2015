f7934f8180784c0d0b462c28f2ad0c381824b705
karieiriks <kari14@ru.is>
2015-05-12 22:20:06 +0000

    lagfæring á user test

--- TravelBookSolution/TravelBookApplication.Tests/Queries/UnitTest4.cs
+++ TravelBookSolution/TravelBookApplication.Tests/Queries/UnitTest4.cs
@@ -12,25 +12,18 @@ namespace TravelBookApplication.Tests.Queries
         [TestMethod]
         public void UserTest()
         {
-<<<<<<< HEAD
+            // Hérna er rétta virknin
+            #region
             MockUserRepository repo = new MockUserRepository();
             ApplicationUser user = new ApplicationUser {Id = "1", Gender = "Male"};
             repo.Save(user);
-=======
-           /* MockUserRepository repo = new MockUserRepository();
-            ApplicationUser user = new ApplicationUser {Id = 1, Gender = "Male"};
-            repo.Users.Add(user);
->>>>>>> bc31232991b2357a2c41093fbc1e9686a8a02d55
-
+           
             Assert.AreEqual(repo.Users.Count, 1);
             var result = repo.GetUserById("1", repo);
             var expected = "1";
-<<<<<<< HEAD
-            Assert.AreEqual(result.Id, expected);
-=======
-            Assert.AreEqual(result, expected);*/
->>>>>>> bc31232991b2357a2c41093fbc1e9686a8a02d55
 
+            Assert.AreEqual(result.Id, expected);
+            #endregion
         }
     }
 }
