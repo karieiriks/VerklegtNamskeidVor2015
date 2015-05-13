997983b25d71dc1157818319bfa865f904967e37
unknown <sverrir1993@gmail.com>
2015-05-13 12:37:33 +0000

    fixed some overflow in css, and started on the stories

---
+++ TravelBookSolution/TravelBookApplication/Scripts/storyscript.js
@@ -0,0 +1,10 @@
+﻿
+$(document).ready(function () {
+
+    createbutton = $("#story-create");
+    createmodal = $("#story-create-modal");
+
+    $(createbutton).click(function(){
+        createmodal.modal("show");
+    })
+})
