f34655d552678cad0678b4bdc1634343ea434e73
AndriM <andrimar1993@gmail.com>
2015-05-14 05:58:29 +0000

    displaying comments like a boss

--- TravelBookSolution/TravelBookApplication/Scripts/CommentDisplay.js
+++ TravelBookSolution/TravelBookApplication/Scripts/CommentDisplay.js
@@ -1,4 +1,12 @@
-﻿$(Document).ready(function() {
-    var id = 
-    $.get("Content/GetAllComments", )
+﻿$(Document).ready(function () {
+    alert("ready");
+    var comment = $("comment-text-area").val();
+    $.ajax({
+        type: "POST",
+        url: $(this).attr("action"),
+        data: $(this).serialize(),
+        success: function(data) {
+            $(".comment-display").append("<br/>" + comment);
+        }
+    });
 });
