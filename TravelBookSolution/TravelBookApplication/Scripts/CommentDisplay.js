42364e3ee7caf91cd91de1acdf23cad5dae7f5ed
AndriM <andrimar1993@gmail.com>
2015-05-14 06:59:29 +0000

    fixed comment owner bug

--- TravelBookSolution/TravelBookApplication/Scripts/CommentDisplay.js
+++ TravelBookSolution/TravelBookApplication/Scripts/CommentDisplay.js
@@ -1,12 +1,14 @@
-﻿$(Document).ready(function () {
+﻿$(document).ready(function (e) {
     alert("ready");
-    var comment = $("comment-text-area").val();
-    $.ajax({
+    e.preventDefault();
+    var comment = document.getElementsByName("comment-text-area")[0].value;
+    alert(comment);
+    /*$.ajax({
         type: "POST",
         url: $(this).attr("action"),
         data: $(this).serialize(),
         success: function(data) {
             $(".comment-display").append("<br/>" + comment);
         }
-    });
+    });*/
 });
