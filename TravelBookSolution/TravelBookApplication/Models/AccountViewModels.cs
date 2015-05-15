cbf6c3cd5bcaa9573409bbec914ae1ad3ee21cbb
AndriM <andrimar1993@gmail.com>
2015-05-15 14:20:33 +0000

    length validate

--- TravelBookSolution/TravelBookApplication/Models/AccountViewModels.cs
+++ TravelBookSolution/TravelBookApplication/Models/AccountViewModels.cs
@@ -77,12 +77,14 @@ namespace TravelBookApplication.Models
 		[Required]
 		[RegularExpression(@"[^0-9]+[àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]*[a-zA-Z]*",
 							ErrorMessage = "Name can not include numbers")]
+		[MaxLength(20, ErrorMessage = "{0} can not be more than 20 characters long")]
 		[Display(Name = "First name")]
 		public string FirstName { get; set; }
 
 		[Required]
 		[RegularExpression(@"[^0-9]+[àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]*[a-zA-Z]*",
 							ErrorMessage = "Name can not include numbers")]
+		[MaxLength(20, ErrorMessage = "Last name can not be more than 20 characters long")]
 		[Display(Name = "Last name")]
 		public string LastName { get; set; }
 		
